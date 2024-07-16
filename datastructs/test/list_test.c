ef1d8e4bce670e1acd7ef903097256b7f19dd273
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-07-16 11:26:23 -0700

    list setup done

--- datastructs/test/list_test.c
+++ datastructs/test/list_test.c
@@ -4,6 +4,28 @@
 int main(void) {
     List* list = list_init(5);
     list_add(list, 5);
+    list_add(list, 7);
+    // [5, 7]
+
+    list_remove_e(list, 5);
+    // [7]
+
+    list_add(list, 5);
+    // [7, 5]
+
+    list_insert(list, 6, 1);
+    // [7, 6, 5]
+    
+    list_insert(list, 4, 0);
+    // [4, 7, 6, 5]
+
+    list_set(list, 3, 0);
+    // [3, 7, 6, 5]
+
+    list_remove_i(list, 1);
+    // [3, 6, 5]
+
     list_print(list);
+    free_list(list);
     return 0;
 }
